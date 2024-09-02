62ba6a7927d67f0f149ec88446f659a382d025ca
Julian Liber <34191597+liberjul@users.noreply.github.com>
2024-09-02 09:46:11 -0400

    Update pull_seq.py to fix.

--- pull_seq.py
+++ pull_seq.py
@@ -11,22 +11,18 @@ parser.add_argument("-p", "--stop", type=int, default = -1, help="1-indexed stop
 parser.add_argument("-m", "--minus_strand", action="store_true", help="If the minus strand should be returned.")
 args = parser.parse_args()
 
-found_contig = False
 with open(args.fasta, "r") as ifile:
     line = ifile.readline()
     while line != "":
         if line[0] == ">":
-            if found_contig:
-                break
-            header = line.strip()
-            if header[1:] == args.contig:
-                found_contig = True
-            seq = ""
-        else:
-            seq += line.strip()
+            if line.strip()[1:] == args.contig:
+                header = args.contig
+                line = ifile.readline()
+                seq = ""
+                while line != "" and line[0] != ">":
+                    seq += line.strip()
+                    line = ifile.readline()
         line = ifile.readline()
-
-print(seq)
 
 if args.stop < 0:
     if args.minus_strand:
